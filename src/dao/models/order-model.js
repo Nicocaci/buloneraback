3015ae6d1932e3c82f79cab7a77960216880cde1
Nicolás Caci <nicko.caci@gmail.com>
2026-06-01 11:28:17 -0300

    push

--- src/dao/models/order-model.js
+++ src/dao/models/order-model.js
@@ -32,7 +32,7 @@ const orderSchema = new mongoose.Schema({
 
   status: {
     type: String,
-    enum: ["pendiente", "enviado", "entregado", "cancelado"],
+    enum: ["pendiente", "enviado", "entregado", "cancelado", "pagado"],
     default: "pendiente",
   },
 
