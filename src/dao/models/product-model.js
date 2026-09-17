efc3a07ba90fd1688fbe84881745ed9cc9c120a4
Nicolás Caci <nicko.caci@gmail.com>
2026-09-17 13:25:41 -0300

    push

--- src/dao/models/product-model.js
+++ src/dao/models/product-model.js
@@ -1,42 +1,41 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    sku: { type: String, required: true, unique: true },
-    item: { type: String, required: true },
-    imagen: { type: [String], required: true },
-    descripcion: { type: String, required: true },
-    marca: { type: String, required: true },
-    categoria: { type: String, required: true },
-    subcategoria: { type: String, required: true },
-    precio: { type: Number, required: true },
-    iva: { type: Number, default: 21 },
-    stock: { type: Number, required: true },
-    estado: { type: String, enum: ["activo", "inactivo"], default: "activo" },
-    peso: { type: Number, default: 1, min: 0 },       // kg
-    alto: { type: Number, default: 20, min: 0 },      // cm
-    ancho: { type: Number, default: 15, min: 0 },     // cm
-    largo: { type: Number, default: 10, min: 0 },     // cm
-    oferta: { 
-        activa: { type:Boolean, default: false},
-        descuento: { type:Number, default:0, min:0, max:100},
-        vence: { type: Date, default: null},
-    },
-    }
-);
+  sku: { type: String, required: true, unique: true },
+  item: { type: String, required: true },
+  imagen: { type: [String], required: true },
+  descripcion: { type: String, required: true },
+  marca: { type: String, required: true },
+  categoria: { type: String, required: true },
+  subcategoria: { type: String, required: true },
+  precio: { type: Number, required: true },
+  iva: { type: Number, default: 21 },
+  stock: { type: Number, required: true },
+  estado: { type: String, enum: ["activo", "inactivo"], default: "activo" },
+  peso: { type: Number, min: 0 }, // kg
+  alto: { type: Number, min: 0 }, // cm
+  ancho: { type: Number, min: 0 }, // cm
+  largo: { type: Number, min: 0 }, // cm
+  oferta: {
+    activa: { type: Boolean, default: false },
+    descuento: { type: Number, default: 0, min: 0, max: 100 },
+    vence: { type: Date, default: null },
+  },
+});
 //Precio con IVA incluido
-// 
-productSchema.virtual('precioConIva').get(function () {
-    return Math.round(this.precio * (1 + this.iva / 100));
+//
+productSchema.virtual("precioConIva").get(function () {
+  return Math.round(this.precio * (1 + this.iva / 100));
 });
 
 productSchema.virtual("precioFinal").get(function () {
-    const base = this.precioConIva;
-    if( !this.oferta.activa || this.oferta.descuento === 0) return base;
-    return Math.round(base * (1 - this.oferta.descuento / 100));
+  const base = this.precioConIva;
+  if (!this.oferta.activa || this.oferta.descuento === 0) return base;
+  return Math.round(base * (1 - this.oferta.descuento / 100));
 });
 
-productSchema.set('toJSON', { virtuals: true});
+productSchema.set("toJSON", { virtuals: true });
 
-const ProductModel = mongoose.model('products', productSchema);
+const ProductModel = mongoose.model("products", productSchema);
 
 export default ProductModel;
